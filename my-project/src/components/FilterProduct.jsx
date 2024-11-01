3efc3b4846552e353358e014992d5b045d510880
Huy Hoàng <tranhuyyhoang2004@gmail.com>
2024-11-02 00:45:40 +0700

    fix: delete csstransaction

--- my-project/src/components/FilterProduct.jsx
+++ my-project/src/components/FilterProduct.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import icons from "../assets/icons";
-import filterProduct from "../css/filterProduct.css";
 
 function FilterProduct() {
   const [showFilterSize, setShowFilterSize] = useState(false);
@@ -20,20 +19,25 @@ function FilterProduct() {
       </div>
 
       {/* Khối giữ không gian cho modal */}
-      <CSSTransition
-        in={showFilterSize}
-        timeout={300}
-        classNames="fade"
-        unmountOnExit
-      >
+      {showFilterSize && (
         <div className="flex flex-wrap items-center justify-between w-full mt-2">
-          <div className="font-sans font-light text-sm border px-2 rounded-md">S</div>
-          <div className="font-sans font-light text-sm border px-2 rounded-md">M</div>
-          <div className="font-sans font-light text-sm border px-2 rounded-md">L</div>
-          <div className="font-sans font-light text-sm border px-2 rounded-md">XL</div>
-          <div className="font-sans font-light text-sm border px-2 rounded-md">XXL</div>
+          <div className="font-sans font-light text-sm border px-2 rounded-md">
+            S
+          </div>
+          <div className="font-sans font-light text-sm border px-2 rounded-md">
+            M
+          </div>
+          <div className="font-sans font-light text-sm border px-2 rounded-md">
+            L
+          </div>
+          <div className="font-sans font-light text-sm border px-2 rounded-md">
+            XL
+          </div>
+          <div className="font-sans font-light text-sm border px-2 rounded-md">
+            XXL
+          </div>
         </div>
-      </CSSTransition>
+      )}
 
       <div className="flex justify-between py-3 items-center">
         <span>MÀU SẮC</span>
